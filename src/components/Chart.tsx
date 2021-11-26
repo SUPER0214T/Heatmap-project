5eb97c0f04d4f2fd4392e8646f31e77102d5b059
JunYoung <twinjyjh2@gmail.com>
2021-11-26 19:30:10 +0900

    Setup Heatmap project

---
+++ src/components/Chart.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ApexChart from 'react-apexcharts';
+
+function Chart() {
+	return <span>Chart Page</span>;
+}
+
+export default Chart;
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+	bgColor: '#130f40',
+	textColor: '#ecf0f1',
+	accentColor: '#2ecc71',
+};
